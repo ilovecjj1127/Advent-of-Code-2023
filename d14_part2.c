fb45e4c1bfdbf207ca7bbb1abe2455860f1a7263
ilovecjj1127 <jc5029@columbia.edu>
2023-12-14 23:43:02 +0100

    d14_part2 unfinished

--- d14_part2.c
+++ d14_part2.c
@@ -1,9 +1,12 @@
 #include "libft/libft.h"
 
+
 void	move_rock_north(char **platform, int i, int j)
 {
 	int pos;
 
+	if (i == 0)
+		return ;
 	pos = i - 1;
 	while (pos > 0 && platform[pos][j] == '.')
 		pos--;
@@ -17,39 +20,45 @@ void	move_rock_south(char **platform, int i, int j)
 {
 	int pos;
 
-	pos = i - 1;
-	while (pos > 0 && platform[pos][j] == '.')
-		pos--;
-	if (pos == 0 && platform[pos][j] == '.')
-		pos--;
+	if (!platform[i + 1])
+		return ;
+	pos = i + 1;
+	while (platform[pos + 1] && platform[pos][j] == '.')
+		pos++;
+	if (!platform[pos + 1] && platform[pos][j] == '.')
+		pos++;
 	platform[i][j] = '.';
-	platform[pos + 1][j] = 'O';
+	platform[pos - 1][j] = 'O';
 }
 
 void	move_rock_east(char **platform, int i, int j)
 {
 	int pos;
 
-	pos = i - 1;
-	while (pos > 0 && platform[pos][j] == '.')
-		pos--;
-	if (pos == 0 && platform[pos][j] == '.')
-		pos--;
+	if (!platform[0][j + 1])
+		return ;
+	pos = j + 1;
+	while (platform[i][pos + 1] && platform[i][pos] == '.')
+		pos++;
+	if (!platform[i][pos + 1] && platform[i][pos] == '.')
+		pos++;
 	platform[i][j] = '.';
-	platform[pos + 1][j] = 'O';
+	platform[i][pos - 1] = 'O';
 }
 
 void	move_rock_west(char **platform, int i, int j)
 {
 	int pos;
 
-	pos = i - 1;
-	while (pos > 0 && platform[pos][j] == '.')
+	if (j == 0)
+		return ;
+	pos = j - 1;
+	while (pos > 0 && platform[i][pos] == '.')
 		pos--;
-	if (pos == 0 && platform[pos][j] == '.')
+	if (pos == 0 && platform[i][pos] == '.')
 		pos--;
 	platform[i][j] = '.';
-	platform[pos + 1][j] = 'O';
+	platform[i][pos + 1] = 'O';
 }
 
 void	tilt_north(char **platform)
@@ -61,11 +70,63 @@ void	tilt_north(char **platform)
 	len = ft_strlen(platform[0]);
 	while (++j < len)
 	{
-		i = 0;
+		i = -1;
 		while (platform[++i])
 		{
 			if (platform[i][j] == 'O')
 				move_rock_north(platform, i, j);
+		}
+	}
+}
+
+void	tilt_south(char **platform, int dep)
+{
+	int	i, j;
+	int	len;
+
+	j = -1;
+	len = ft_strlen(platform[0]);
+	while (++j < len)
+	{
+		i = dep;
+		while (--i >= 0)
+		{
+			if (platform[i][j] == 'O')
+				move_rock_south(platform, i, j);
+		}
+	}
+}
+
+void	tilt_west(char **platform)
+{
+	int	i, j;
+	int	len;
+
+	j = -1;
+	len = ft_strlen(platform[0]);
+	while (++j < len)
+	{
+		i = -1;
+		while (platform[++i])
+		{
+			if (platform[i][j] == 'O')
+				move_rock_west(platform, i, j);
+		}
+	}
+}
+
+void	tilt_east(char **platform)
+{
+	int	i, j;
+
+	j = ft_strlen(platform[0]);
+	while (--j >= 0)
+	{
+		i = -1;
+		while (platform[++i])
+		{
+			if (platform[i][j] == 'O')
+				move_rock_east(platform, i, j);
 		}
 	}
 }
@@ -108,7 +169,14 @@ int	main(int argc, char **argv)
 	}
 	platform = ft_split(content, '\n');
 	int	dep = arr_len(content, '\n');
-	tilt_north(platform);
+	int	i = 0;
+	while (++i <= 1000000000)
+	{
+		tilt_north(platform);
+		tilt_west(platform);
+		tilt_south(platform, dep);
+		tilt_east(platform);
+	}
 	printf("%d\n", get_load(platform, dep));
 
 	free(content);
